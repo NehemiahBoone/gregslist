aa38e2fe36fa09b0834cb18a62fa68a975bbb896
Nehemiah Boone <nehemiah.boone@gmail.com>
2020-08-26 15:44:08 -0600

    job cards are drawn

--- app/Controllers/JobsController.js
+++ app/Controllers/JobsController.js
@@ -2,9 +2,14 @@ import jobsService from "../Services/JobsService.js"
 import STORE from "../store.js"
 
 function _drawJobs() {
-
+  let jobs = STORE.State.jobs
+  let template = ''
+  jobs.forEach(j => template += j.Template)
+  document.getElementById('jobs').innerHTML = template
 }
 
 export default class JobsController {
-
+  constructor() {
+    _drawJobs()
+  }
 }
